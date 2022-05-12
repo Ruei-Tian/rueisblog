717f03ba5f4a8faef750d8684cade82a0d7534ee
Ruei <ruei.wang@104.com.tw>
2022-05-12 21:09:40 +0800

    [init] try React hook

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,66 @@
-import React from 'react';
-import ReactDom from 'react-dom';
+import React, { useState, useEffect } from 'react';
+import { createRoot } from 'react-dom/client';
+import PropTypes from 'prop-types';
 import styles from './index.scss';
 
-function Main() {
-  return <h1 className={styles.main}>Hi JSX！</h1>;
-}
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  const clickHandler = () => {
+    setCount(count + 1);
+  };
 
-ReactDom.render(<Main />, document.getElementById('app'));
+  const [name, setName] = useState('ruei');
+
+  useEffect(() => {
+    console.log('after', count, name);
+    return () => {
+      console.log('before', count, name);
+    };
+  }, [count]);
+
+  return (
+    <>
+      <p>{count}</p>
+      <button type="button" onClick={clickHandler}>change Count!</button>
+      <p>{name}</p>
+      <button type="button" onClick={() => setName('Peter')}>change Name!</button>
+    </>
+  );
+};
+
+const CountHandler = () => {
+  const [ifCounterShow, setIfCounterShow] = useState(true);
+
+  return (
+    <>
+      {ifCounterShow ? <Counter /> : null}
+      <button type="button" onClick={() => setIfCounterShow(!ifCounterShow)}>toggle Counter!</button>
+    </>
+  );
+};
+const SayHello = (props) => {
+  const { names } = props;
+
+  return (
+    <div
+      className={styles.main}
+      style={
+      {
+        fontSize: 30,
+      }
+    }
+    >
+      {names.map((name) => (<p key={name}>{`Hello ${name || 'world'}`}</p>))}
+    </div>
+  );
+};
+
+SayHello.propTypes = {
+  names: PropTypes.arrayOf(PropTypes.string),
+};
+SayHello.defaultProps = {
+  names: [],
+};
+
+const root = createRoot(document.getElementById('app'));
+root.render(<CountHandler />);
